f0bec2e818e45871c6672330ac6776eb8f8d764e
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-12-16 11:29:48 -0600

    create and pass 4th test

--- Order-tracker.Tests/ModelTests/VendorTests.cs
+++ Order-tracker.Tests/ModelTests/VendorTests.cs
@@ -49,5 +49,22 @@ namespace Tracker.Tests
       Assert.AreEqual(1, result);
     }
 
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      //Arrange
+      string name01 = "Starbucks";
+      string name02 = "Wendys";
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+
+      //Act
+      List<Vendor> result = Vendor.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
   }
 }
