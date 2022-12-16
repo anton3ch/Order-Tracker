0d65900b6e3ebfe73bb5a01cf29563605cf8c08f
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-12-16 10:49:11 -0600

    create and pass the first test

---
+++ Order-tracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Tracker.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+  }
+}
---
+++ Order-tracker/Models/Vendor.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace Tracker.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+
+    public Vendor(string categoryName)
+    {
+      Name = categoryName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order>{};
+    }
+  }
+}
