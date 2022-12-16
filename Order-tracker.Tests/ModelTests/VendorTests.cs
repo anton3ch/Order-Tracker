85910b58e6141eff497f5c215ec49b770b2d7289
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-12-16 11:01:11 -0600

    create and pass 2nd test

--- Order-tracker.Tests/ModelTests/VendorTests.cs
+++ Order-tracker.Tests/ModelTests/VendorTests.cs
@@ -16,5 +16,19 @@ namespace Tracker.Tests
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
 
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
+
+      //Act
+      string result = newVendor.Name;
+
+      //Assert
+      Assert.AreEqual(name, result);
+    }
+
   }
 }
